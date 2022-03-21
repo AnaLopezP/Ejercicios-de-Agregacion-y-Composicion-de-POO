98eb247f5ddddbd455bb1ee66371eb7a8f53a04f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-21 19:57:23 +0100

    clase destruccion NY

--- El_dia_siguiente.py/El_dia_siguiente.py
+++ El_dia_siguiente.py/El_dia_siguiente.py
@@ -1,3 +1,6 @@
+from re import A
+
+
 class CIUDAD:
     def __init__(self, NY, LA):
         self.NY = NY
@@ -10,9 +13,22 @@ class EDIFICIO(CIUDAD):
         self.B = B
         self.C = C
 
+
 class EMPRESA(EDIFICIO):
     def __init__(self, NY, LA, A, B, C, martin, salim, xing):
         super().__init__(NY, LA, A, B, C)
         self.martin = martin
         self.salim = salim
         self.xing = xing 
+
+edificaciones = EDIFICIO()
+ciudades = CIUDAD()
+trabajo = EMPRESA()
+class destrucc_NY(EMPRESA):
+    def __init__(self, NY, LA, A, B, C, martin, salim, xing):
+        super().__init__(NY, LA, A, B, C, martin, salim, xing)
+
+    def __del__(self):
+        ciudades.remove(self.NY)
+        edificaciones.remove(self.A, self.B)
+        
