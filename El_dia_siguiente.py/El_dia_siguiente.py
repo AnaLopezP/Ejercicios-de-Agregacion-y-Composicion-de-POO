57ad6cd43bdc1431a825273924831390bb999a87
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-21 19:32:52 +0100

    clase edificio

--- El_dia_siguiente.py/El_dia_siguiente.py
+++ El_dia_siguiente.py/El_dia_siguiente.py
@@ -3,4 +3,9 @@ class CIUDAD:
         self.NY = NY
         self.LA = LA
 
-    
+class EDIFICIO(CIUDAD):
+    def __init__(self, NY, LA, A, B, C):
+        super().__init__(NY, LA)
+        self.A = A
+        self.B = B
+        self.C = C
