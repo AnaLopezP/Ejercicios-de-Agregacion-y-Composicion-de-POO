148b3a86abd2c321538a51471ba34d9419cf5456
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-22 19:51:48 +0100

    codigo principal bonito :)

--- El_dia_siguiente.py/El_dia_siguiente.py
+++ El_dia_siguiente.py/El_dia_siguiente.py
@@ -10,6 +10,9 @@ class Ciudad:
     
     def get_edifs(self):
         return self.edificios
+    def __del__(self):
+        self.nombre = ""
+        self.edificios = []
 
 class Edificio:
     nombre = ""
@@ -46,15 +49,42 @@ ny.add_edif(edif_a)
 ny.add_edif(edif_b)
 la.add_edif(edif_c)
 
+print("Estas son las ciudades que hay:")
 print(ny.nombre)
-print(ny.edificios.pop().get_nombre())
-print(ny.edificios.pop().get_nombre())
-print(la.edificios.pop().get_nombre())
+print(la.nombre)
+print("Este es el nombre de la empresa:")
 print(yoohoo.nombre)
+print("Los edificios que tiene a su propiedad:")
 print(yoohoo.list_edificios.pop().get_nombre())
 print(yoohoo.list_edificios.pop().get_nombre())
 print(yoohoo.list_edificios.pop().get_nombre())
+print("Estos son los empleados que trabajan en esta empresa:")
 print(yoohoo.list_empleados.pop().nombre)
 print(yoohoo.list_empleados.pop().nombre)
 print(yoohoo.list_empleados.pop().nombre)
+print("Estos son los edificios que hay en Nueva York pertenecientes a Yoohoo!:")
+print(ny.edificios.pop().get_nombre())
+print(ny.edificios.pop().get_nombre())
+print("Estos son los edificios que hay en Los Angeles pertenecientes a Yoohoo!:")
+print(la.edificios.pop().get_nombre())
+
+
+print("¿Quiere destruir Nueva York?  s/n")
+respuesta = input()
+if respuesta == "s":
+    ny.__del__()
+    if len(ny.nombre) == 0:
+        print("Ciudades de Nueva York restantes: " + str(len(ny.nombre)))
+        print("La ciudad de Nueva York ya no existe, ha sido destruida")
+    else:
+        print("Ha habido un error al pulsar el boton rojo. No se ha podido destruir.")
+    if len(ny.edificios) == 0:
+        print("Edificios de Yoohoo restantes: " + str(len(ny.edificios)))
+        print("Los edificios de Nueva York han sido destruidos")
+    else:
+        print("Ha habido un error al pulsar el boton rojo. No se ha podido destruir.")
+elif respuesta == "n":
+    print("De acuerdo.")
+else:
+    print("Esa no es una opcion valida")
 
