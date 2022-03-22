ce4b7e423f4529e23b5fd51583fb7da8bfae1095
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-22 18:46:57 +0100

    construccion objetos

--- El_dia_siguiente.py/El_dia_siguiente.py
+++ El_dia_siguiente.py/El_dia_siguiente.py
@@ -1,19 +1,46 @@
 
-class CIUDAD:
+class Ciudad:
     def __init__(self, nombre, list_edificios):
         self.nombre = nombre
         self.list_edificios = list_edificios
+    
+    def __init__(self, nombre):
+        self.nombre = nombre
 
-class EDIFICIO:
-    def __init__(self, nombre_ciudad):
-        self.nombre_ciudad = nombre_ciudad
+    def add_edif(edif):
+        list_edificios.append(edif)
+
+class Edificio:
+    def __init__(self, nombre_edif):
+        self.nombre_edif = nombre_edif
 
 
-class EMPRESA:
+class Empresa:
     def __init__(self, nombre, list_empleados, list_edificios):
         self.nombre = nombre
         self.list_empleados = list_empleados
         self.list_edificios = list_edificios
+
+class Empleado:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    
+martin = Empleado("Martin")
+salim = Empleado("Salim") 
+xing = Empleado("Xing")
+edif_a = Edificio("A")
+edif_b = Edificio("B")
+edif_c = Edificio("C")
+list_edificios = [edif_a, edif_b, edif_c]
+list_empleados = [martin, salim, xing]
+yoohoo = Empresa("Yoohoo!", list_empleados, list_edificios)
+ny = Ciudad("Nueva York")
+la = Ciudad("Los Angeles")
+ny.add_edif(edif_a)
+ny.add_edif(edif_b)
+la.add_edif(edif_c)
+
 
 '''edificaciones = EDIFICIO()
 los_angeles = CIUDAD("Los Angeles")
