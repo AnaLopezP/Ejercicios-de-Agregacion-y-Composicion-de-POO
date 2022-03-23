c5866eafa91cb8b2fd90a96b7ca00a326dca1f1f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-23 19:28:32 +0100

    clase cristal bien hecha

--- Alternativa/alternativa.py
+++ Alternativa/alternativa.py
@@ -1,10 +1,57 @@
+
+
+
 class Cristal:
+    def __init__(self, alto, ancho):
+        self.alto = alto
+        self.ancho = ancho
+        self.superficie = alto * ancho
+    
+    def __init__(self):
+        self.alto = 0
+        self.ancho = 0
+        self.superficie = 0
+
+    def get_superficie(self):
+        return self.superficie
+    
+    def get_ancho(self):
+        return self.ancho
+
+    def get_alto(self):
+        return self.alto
+
+    def set_alto(self, alto):
+        self.alto = alto
+
+    def set_ancho(self, ancho):
+        self.ancho = ancho
+
+class Ventana(Cristal):
     def __init__(self, medidas):
         self.medidas = medidas
-        medidas = []
-    def superficie_acristalada(self, medidas):
-        superficie = sum(medidas)
-        return superficie
+        
+
+class Pared(Cristal): pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #funcion que mira la proteccion que quiere en las ventanas
 def proteccion():
@@ -22,3 +69,4 @@ def proteccion():
             print("gracias. Siguiente paso")
     else:
         raise Exception("No se ha escogido ninguna proteccion")
+
