a2feedc79e20fa3e1ec60713feaaebd3c344e690
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-23 18:18:58 +0100

    explicacion por que

--- Inmortal.py/inmortal.py
+++ Inmortal.py/inmortal.py
@@ -1,1 +1,4 @@
 #si quitamos el yin.yang = yang (asi como el print(yin.yang is yang) para que no de error), el mensaje aparece antes de la interrogacion.
+#esto ocurre porque al hacer esa asignacion creamos dos variables apuntando al mismo objeto.
+#entonces, al destruirlo, destruye una de las dos variables, pero no el objeto, que se destruye automaticamente al final de la ejecucion.
+#otra manera de evitar esto es llamando al destructor con las dos variables antes de la interrogacion.
