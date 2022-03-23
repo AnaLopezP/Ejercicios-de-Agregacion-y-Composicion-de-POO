7c58820d642d988be123135591153e37b25ff6cf
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-03-23 19:59:22 +0100

    recalculo de superficie en setter

--- Alternativa/alternativa.py
+++ Alternativa/alternativa.py
@@ -5,7 +5,7 @@ class Cristal:
     def __init__(self, alto, ancho):
         self.alto = alto
         self.ancho = ancho
-        self.superficie = alto * ancho
+        self.superficie = self.alto * self.ancho
     
     '''def __init__(self):
         self.alto = 0
@@ -24,9 +24,15 @@ class Cristal:
 
     def set_alto(self, alto):
         self.alto = alto
+        self.superficie = self.alto * self.ancho
 
     def set_ancho(self, ancho):
         self.ancho = ancho
+        self.superficie = self.alto * self.ancho
+    
+    def __str__(self):
+        cadena = "ancho = " + str(self.ancho) + " alto = " + str(self.alto) + " superficie = " + str(self.superficie)
+        return cadena
 
 class Ventana(Cristal):
     def __init_subclass__(self, alto, ancho):
@@ -38,6 +44,9 @@ class Ventana(Cristal):
     def set_estado(self, estado):
         self.estado = estado
         
+    def __str__(self) -> str:
+        cadena = super().__str__() + " estado = " + str(self.estado)
+        return cadena
 
 class Pared(Cristal): 
     def __init_subclass__():
@@ -46,16 +55,27 @@ class Pared(Cristal):
     def get_opacidad(self):
         return self.opacidad
     
-    def set_estado(self, opacidad):
+    def set_opacidad(self, opacidad):
         self.opacidad = opacidad
+    
+    def __str__(self) -> str:
+        cadena = super().__str__() + " opacidad = " + str(self.opacidad)
+        return cadena
 
 ventana = Ventana(6, 6)
 print(ventana.get_superficie())
 ventana.set_alto(5)
 ventana.set_ancho(5)
-print(ventana.get_superficie())
+ventana.set_estado("abierta")
+print(ventana)
+#print(ventana.get_superficie())
 
-
+pared = Pared(3, 6)
+print(pared.get_superficie())
+pared.set_alto(8)
+pared.set_ancho(5)
+pared.set_opacidad("translucido")
+print(pared)
 
 
 
